6cdcdea316ed0afc832ca761c56ed0ca7d0ea676
Eden <bahalula@gmail.com>
2020-06-18 17:04:06 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const { profile } = require('console');
+const port = 3000
+const pages_dir = path.resolve(__dirname, "..", "client", "pages");
+const public_dir = path.resolve(__dirname, "..", "client", "public");
+
+
+
+app.use(express.static(public_dir))
+app.use(express.urlencoded({ extended: false }));
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(pages_dir, 'home.html'))
+})
+
+app.get('/login', (req, res) => {
+    /* todo */
+    res.sendFile(path.join(pages_dir, 'login.html'))
+})
+app.post('/login', (req, res) => {
+    /* todo */
+    res.sendFile(path.join(pages_dir, 'login.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(pages_dir, 'register.html'))
+})
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.sendFile(path.join(pages_dir, 'register.html'))
+})
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(pages_dir, 'profile.html'))
+})
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`))
